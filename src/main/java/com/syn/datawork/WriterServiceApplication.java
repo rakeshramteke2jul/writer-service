a75c9d5dcd1fd1cdc0bc7e695507878714cd2253
rakeshramteke2jul <ramteke.rakesh@gmail.com>
2020-10-06 09:16:55 -0700

    commit files

---
+++ src/main/java/com/syn/datawork/WriterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syn.datawork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterServiceApplication.class, args);
+	}
+
+}
